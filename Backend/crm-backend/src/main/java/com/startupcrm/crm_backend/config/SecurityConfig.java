d69eb2cb9c657cde9ea495ca493031c7f91051b2
IngRodrigoPena <ingrodrigopena@gmail.com>
2026-04-11 13:43:31 -0600

    Agregar configuracion de CORS  para deployment con Render

--- Backend/crm-backend/src/main/java/com/startupcrm/crm_backend/config/SecurityConfig.java
+++ Backend/crm-backend/src/main/java/com/startupcrm/crm_backend/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.startupcrm.crm_backend.config;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import java.util.List;
+
 
 @Configuration
 @EnableWebSecurity
@@ -15,10 +22,36 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**").permitAll()   // permite todo
                 );
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOriginPatterns(List.of(
+                "https://*.vercel.app",
+                "http://localhost:5173"
+        ));
+
+        config.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", config);
+
+        return source;
+    }
+
+
 }
